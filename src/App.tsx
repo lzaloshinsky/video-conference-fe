ddb7857d59eb8ddd8a5a7220c615d5292807b56c
lzaloshinsky <l0548400737@gmail.com>
2024-07-17 02:37:21 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from "./data"; // TODO fetch from BE
+import { useState } from "react";
+import Header from "./components/Header";
+import SideBar from "./components/Sidebar";
+import Conference from "./components/Conference";
 
-function App() {
+const App = () => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const {
+    topic,
+    judges,
+    parties,
+    committee,
+    startDate,
+    caseNumber,
+    isRecording,
+    id: meetingId,
+  } = data;
+
+  const allParticipants = [...judges, ...parties];
+
+  const currentParticipantId = allParticipants.find(
+    (participant) => participant.isCurrent
+  )?.id;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`h-screen bg-sky-950 flex ${isFullScreen ? "" : "py-3 pl-3"}`}
+    >
+      {!isFullScreen && (
+        <SideBar
+          meetingId={meetingId}
+          participantsCount={allParticipants.length + 1}
+        />
+      )}
+
+      <div
+        className={`flex flex-col h-full w-full relative justify-between ${
+          isFullScreen ? "" : "mr-4"
+        }`}
+      >
+        {!isFullScreen && (
+          <Header
+            topic={topic}
+            committee={committee}
+            caseNumber={caseNumber}
+            isRecording={isRecording}
+            isFullScreen={isFullScreen}
+            onShowFullScreen={() => setIsFullScreen(true)}
+          />
+        )}
+
+        <Conference
+          judges={judges}
+          parties={parties}
+          committee={committee}
+          startDate={startDate}
+          meetingId={meetingId}
+          isFullScreen={isFullScreen}
+          currentParticipantId={currentParticipantId}
+          onCloseFullscreenClick={() => setIsFullScreen(false)}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
